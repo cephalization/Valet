71bcf542e6c6d903ba5d1bc0213a8b6a6a58654b
Tony Powell <adpowell@mtu.edu>
2016-08-22 08:00:34 -0400

    Html injection controller created. 	Angular and Node js separated into different files. 	Express static routes need to be shortened.

---
+++ app.module.js
@@ -0,0 +1,7 @@
+var app = angular.module('valetState', ['ngResource']);
+
+// stateCtrl
+app.controller("stateCtrl", function ($scope) {
+// Select valid html stuff method
+$scope.currentHtml = "login.html";
+});
---
+++ server.js
@@ -0,0 +1,27 @@
+
+// Initialize library objects for use by node
+// Also specify server dependencies
+var express = require('express');
+var server = express();
+var path = require('path');
+
+// Setup file routing
+server.use('/src/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+server.use('/src/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+server.use('/src/css', express.static(__dirname + '/css'));
+server.use('/src/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+server.use('/src/scripts', express.static(__dirname + '/node_modules/angular'));
+server.use('/src/scripts', express.static(__dirname + '/node_modules/angular-route'));
+server.use('/src/scripts', express.static(__dirname + '/node_modules/angular-resource'));
+server.use('/src/scripts', express.static(__dirname + '/controller'));
+server.use('/src/scripts', express.static(__dirname + '/'));
+server.use(express.static(__dirname + '/partials'))
+
+// Setup request functions
+server.listen(3000, function() {
+  console.log('Connection Successful!');
+})
+
+server.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
