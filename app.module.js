45dcf05fe9220fa8ac6260117f5ee809db1389db
Tony Powell <adpowell@mtu.edu>
2017-05-21 15:08:33 -0400

    Added responsive behaviors to navbar and sidenav
    TODO: Style nav elements

--- app.module.js
+++ app.module.js
@@ -136,6 +136,7 @@ app.controller('titleCtrl', function ($scope, $http, $window, $location) {
 		$scope.auth = response.data.auth;
 	});
 
+	console.log('ng scope', $scope);
 	$scope.whichRoot = function () {
 		if ($scope.auth != null) {
 			if ($scope.auth) {
@@ -148,6 +149,15 @@ app.controller('titleCtrl', function ($scope, $http, $window, $location) {
 			}
 		}
 		return '/#!/';
+	};
+
+	$scope.toggleSidenav = function() {
+		var sidenav = document.getElementById('sidenav');
+		if (sidenav.classList.contains('collapse')) {
+			sidenav.classList.remove('collapse');
+		} else {
+			sidenav.classList.add('collapse');
+		}
 	};
 });
 
